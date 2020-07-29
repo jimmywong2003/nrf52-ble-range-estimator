f5b6439b89b470b00df36c044edcc743eb1b987f
jimmy <jimmywong2003@gmail.com>
2020-07-29 14:36:32 +0800

    pre-compile hex on pca10100 board

---
+++ demo_hex/sdk_17.0/program_central_pca10100.bat
@@ -0,0 +1,1 @@
+nrfjprog --program combine_ble_app_uart_c_central_pca10100_s140.hex --reset --chiperase 
---
+++ demo_hex/sdk_17.0/program_peripheral_pca10100.bat
@@ -0,0 +1,1 @@
+nrfjprog --program combine_ble_app_uart_peripheral_pca10100_s140.hex --reset --chiperase 
